ba83e6edda720f7ba10b172711929afaf29f9f9b
user-1 <user-1@MacBook-Pro.local>
2020-08-10 22:09:45 -0600

    finishing step 5 with errors

--- src/redux/userReducer.js
+++ src/redux/userReducer.js
@@ -1,12 +1,33 @@
-// import {  } from "";
+import axios from "axios";
 
 const initialState = {
     email: null,
     firstName: null,
     lastName: null
+};
+
+//I don't understand what this does yet. It's the action type. Review this.
+const REQUEST_USER_DATA = "REQUEST_USER_DATA";
+
+//action creator
+export const requestUserData = () => {
+    let data = axios.get('/auth/user-data').then(res => res.data)
+    return {
+        type: REQUEST_USER_DATA,
+        payload: data
+    }
 }
 
+//reducer function
 export default function reducer(state = initialState, action){
-    return state
-}
+    console.log("ACTION", action)
+    switch(action.type){
+        case REQUEST_USER_DATA + '_FULFILLED':
+            const {email, firstName, lastName} = action.payload.user;
+            return {email, firstName, lastName};
+        // case REQUEST_USER_DATA + "_PENDING":
+        default:
+            return state;
 
+    }
+}
